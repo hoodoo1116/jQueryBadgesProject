6873a3f3a6d6bf4c7ff261d8c87f7f3d88387bf6
nicwas <nickolas.waskiewicz@extendhealth.com>
2018-04-06 14:54:14 -0700

    Add badges project

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,22 @@
-$(function() {
+$(document).ready(function() {
 
   // your code will go here
-
+  // https://www.codeschool.com/users/3553694.json
+  $.ajax({
+    url: 'https://www.codeschool.com/users/3553694.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      console.log(response);
+      // handle response
+      var listItems = $.map(response.courses.completed, function(item, index) {
+        var content = $('<div></div>');
+        content.addClass('course');
+        $('<h3>' + item.title + '</h3>').appendTo(content);
+        $('<img src="'+ item.badge + '" />').appendTo(content);
+        $('<a class="btn btn-primary" target="_blank" href="' + item.url + '">See Course</a>').appendTo(content);
+        return content;
+      });
+      $('#badges').html(listItems);
+    }
+  });
 });
